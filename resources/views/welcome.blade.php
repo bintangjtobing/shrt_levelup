8c3bceedf573d8afa004b05a7b02af3fbbff28a1
Bintang Tobing <hello@bintangtobing.com>
2024-11-05 23:30:42 +0700

    update: welcome and styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Questrial&display=swap"
+        rel="stylesheet">
     <link rel="shortcut icon"
         href="https://res.cloudinary.com/boxityapp/image/upload/v1730810393/levelupgaming/nupfkufgxx6oigtb75ih.png"
         type="image/png">
@@ -54,14 +56,9 @@
         <header class="d-flex flex-wrap align-items-center justify-content-between py-3 mb-4 border-bottom">
             <!-- Logo LevelUp Game Hub -->
             <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-                <img src="https://res.cloudinary.com/boxityapp/image/upload/v1730810393/levelupgaming/zgsvozukgxyrpnybvg22.png"
+                <img src="https://res.cloudinary.com/boxityapp/image/upload/v1730810393/levelupgaming/sokyhylkcybhligfkv2u.png"
                     alt="LevelUp Game Hub Logo" class="img-fluid" style="width: 200px;">
             </a>
-
-            <!-- Button "Need help?" -->
-            <div class="col-md-3 text-end">
-                <button type="button" class="btn btn-primary">Need help?</button>
-            </div>
         </header>
     </div>
 
